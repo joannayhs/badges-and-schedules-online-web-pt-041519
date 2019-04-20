16ecabf2e0b2a5aecb3c1e8e2fd6f26a590748c8
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 18:03:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end 
+end 
+
+def assign_rooms(attendees)
+  attendees.each_with_index.collect do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end 
+
+def printer(attendees)
+  assign_rooms(attendees).each do |greeting|
+    puts greeting
+  end
+end 
